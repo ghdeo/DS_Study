178c24f5896c216490fe04f6e72777346e516b97
rilato <87744890+rilato@users.noreply.github.com>
2021-11-22 18:26:42 +0900

    Update whw3.c

--- whw3/harok/whw3.c
+++ whw3/harok/whw3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#define HASH_PRIME 11117
+#define HASH_PRIME 11117                          // 취향차이겠지만 HASH_PRIME에 값을 바꿔가면서 작성하는 것보다, 한번에 결과 값을 볼 수 있게 했으면 어땠을지
 
 int hash_record[HASH_PRIME];
 
@@ -32,7 +32,7 @@ int sum_col(int n)
   for (i=0; i<n; i++)
   {
     if (hash_record[i] != 0)
-      col += hash_record[i]-1;
+      col += hash_record[i]-1;                  // 제 코드에 비해 효율적으로 짜신 것 같음
   }
   return col;
 }
@@ -42,10 +42,10 @@ int key_comparisons()
   int i;
   int sum = 0;
   for (i=0; i<HASH_PRIME; i++)
-    sum += hash_record[i]*(hash_record[i]+1)/2;
+    sum += hash_record[i]*(hash_record[i]+1)/2; // 취향 차이일 수도 있지만, 가독성을 위해 보통 연산자는 띄어쓰기를 함
   return sum;
 }
-    
+
 
 int main()
 {
@@ -55,7 +55,8 @@ int main()
   init_record();
   FILE *f;
   f=fopen("words.dat","r");
-  for (i=0; i<5757; i++)
+                                                  // 파일이 열리지 않았을 경우 exit(-1)코드
+  for (i=0; i<5757; i++)                          // 여기서 5757로 하면 문제 발생, dat 맨 앞이 *로 시작하는 것은 알아서 제외가 안됨. printf(    fgets(word,sizeof(word), f)); 찍어보기
   {
     fgets(word,sizeof(word), f);
     hash_record[hash(word)] += 1;
